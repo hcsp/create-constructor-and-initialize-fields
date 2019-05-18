1fbccbd4e9b4b9e2a4719796f94f4b7102f99f24
Bo Zhang <bo@gradle.com>
2019-05-18 21:22:57 +0800

    Fix incorrect Home.java locations

---
+++ src/main/java/com/github/hcsp/Home.java
@@ -0,0 +1,12 @@
+package com.github.hcsp;
+
+import com.github.hcsp.pet.Cat;
+
+public class Home {
+    public static void main(String[] args) {
+        // Create a constructor Cat(String name) in Cat.java
+        // 在Cat.java中创建一个构造器Cat(String name)
+        System.out.println(new Cat("Black"));
+        System.out.println(new Cat("White"));
+    }
+}
