af7834764f711db0d71cdcf469a84f7709b72b51
Bo Zhang <bo@gradle.com>
2019-05-18 20:19:56 +0800

    More repositories

---
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -0,0 +1,13 @@
+package com.github.hcsp.pet;
+
+public class Cat {
+    private String name;
+
+    // Create a constructor here
+    // 在这里创建一个构造器
+
+    @Override
+    public String toString() {
+        return "Cat(" + name + ")";
+    }
+}
