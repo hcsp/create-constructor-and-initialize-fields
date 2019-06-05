949ca57649c7e7dd027a8a11d86ceaee1964420b
Administrator <908380434@qq.com>
2019-06-05 08:55:48 +0800

    在Cat类中新建一个构造器

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -1,10 +1,14 @@
 package com.github.hcsp.pet;
 
 public class Cat {
+    // 静态成员变量，不和任何的对象绑定
     private String name;
 
     // Create a constructor here
-    // 在这里创建一个构造器
+    // 在这里创建一个构造器 constructor
+    public Cat(String name) {
+        this.name = name;
+    }
 
     @Override
     public String toString() {
