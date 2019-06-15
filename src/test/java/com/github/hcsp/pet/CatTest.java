4696d37af0441c94d916a30f95b48f64b2720422
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:06 +0800

    Update repo

--- src/test/java/com/github/hcsp/pet/CatTest.java
+++ src/test/java/com/github/hcsp/pet/CatTest.java
@@ -1,12 +1,11 @@
 package com.github.hcsp.pet;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CatTest {
     @Test
     public void catConstructorTest() {
-        assertEquals("Cat(Tom)", new Cat("Tom").toString());
+        Assertions.assertEquals("Cat(Tom)", new Cat("Tom").toString());
     }
 }
