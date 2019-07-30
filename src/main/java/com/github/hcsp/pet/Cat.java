7cb7a2621a49fe65bdd846b0b11805060dc6150f
ziwei <18500905537@163.com>
2019-07-30 11:00:17 +0800

    给Cat写一个带参数的构造函数，并给name赋值为参数name

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -5,6 +5,10 @@ public class Cat {
 
     // Create a constructor here
     // 在这里创建一个构造器
+    public Cat(String name){
+        // 构造器里，给实例设置name,为传入的name
+        this.name = name;
+    }
 
     @Override
     public String toString() {
