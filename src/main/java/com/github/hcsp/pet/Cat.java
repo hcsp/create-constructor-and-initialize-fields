27b91d620987cdd365a5004af179126cc3fe90f6
zhouchangde <55309927+zhouchangde@users.noreply.github.com>
2019-09-21 22:16:36 +0600

    lianxi (#78)
    lianxi (#78)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -2,12 +2,14 @@ package com.github.hcsp.pet;
 
 public class Cat {
     private String name;
-
     // Create a constructor here
     // 在这里创建一个构造器
 
     @Override
     public String toString() {
         return "Cat(" + name + ")";
+    }
+    public Cat(String name){
+        this.name = name;
     }
 }
