c6dec9b131320d982e5207d416fa74da455ddddd
xuepan-summer <54898607+xuepan-summer@users.noreply.github.com>
2019-09-26 13:31:25 +0800

    创建构造器 (#80)
    创建构造器 (#80)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -4,7 +4,11 @@ public class Cat {
     private String name;
 
     // Create a constructor here
-    // 在这里创建一个构造器
+    // 在这里创建一个构造器\
+    public Cat(String name){
+        //this指的是该类里面的name，name是这个传入的参数
+        this.name = name;
+    }
 
     @Override
     public String toString() {
