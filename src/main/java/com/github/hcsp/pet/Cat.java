5d2fecd82c1809da3adc121bc268a8713bb02839
yujie-zhang <907420318@qq.com>
2020-04-01 19:18:16 +0800

    新建一个构造器

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -1,13 +1,16 @@
 package com.github.hcsp.pet;
 
 public class Cat {
-    private String name;
+    private String b;
+    public Cat(String a) {
+        b = a;
+    }
 
     // Create a constructor here
     // 在这里创建一个构造器
 
     @Override
     public String toString() {
-        return "Cat(" + name + ")";
+        return "Cat(" + b + ")";
     }
 }
