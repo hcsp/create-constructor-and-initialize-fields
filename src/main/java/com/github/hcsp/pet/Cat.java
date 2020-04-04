e7924e92a386b1a1d780f9ae9fc960e9727bddcc
刘雨晨 <liuyuchen@cibfintech.com>
2020-04-04 20:01:11 +0800

    work: create a ctor

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -4,6 +4,10 @@ public class Cat {
     private String name;
 
     // Create a constructor here
+
+    public Cat(String name) {
+        this.name = name;
+    }
     // 在这里创建一个构造器
 
     @Override
