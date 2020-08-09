3adcaabe897d582bbf93201934b96abd74f6864b
neverland <zink_zzz@163.com>
2020-08-09 22:38:56 +0800

    create constructor (#217)
    create constructor (#217)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -1,13 +1,16 @@
 package com.github.hcsp.pet;
 
 public class Cat {
-    private String name;
+  private String name;
 
-    // Create a constructor here
-    // 在这里创建一个构造器
+  // Create a constructor here
+  // 在这里创建一个构造器
+  public Cat(String name) {
+    this.name = name;
+  }
 
-    @Override
-    public String toString() {
-        return "Cat(" + name + ")";
-    }
+  @Override
+  public String toString() {
+    return "Cat(" + name + ")";
+  }
 }
