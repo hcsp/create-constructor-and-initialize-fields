f848a0b67dcb1a2f823bd29faff55f8019065e69
YJL <2567843704@qq.com>
2021-09-22 14:32:40 +0800

    2

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -4,11 +4,11 @@ public class Cat {
     private final String name;
     // Create a constructor here
     // 在这里创建一个构造器
-    public Cat (String name){
+    public Cat (String name) {
        this.name = name;
     }
     @Override
     public String toString() {
-        return "Cat(" + name + ")";
+        return "Cat("+ name +")";
     }
 }
