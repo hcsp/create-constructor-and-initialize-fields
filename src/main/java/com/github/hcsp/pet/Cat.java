e8cc73af38e8cf7f089ee182ed3ce962c61c0860
wang <wanggenwang@nuobei.cn>
2022-08-26 20:52:38 +0800

    新建一个构造器

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -5,6 +5,9 @@ public class Cat {
 
     // Create a constructor here
     // 在这里创建一个构造器
+    public Cat(String name) {
+        this.name = name;
+    }
 
     @Override
     public String toString() {
